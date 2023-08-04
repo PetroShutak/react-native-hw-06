002fff1f2b387488600790acc4b535de3538f4c9
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-08-04 19:49:04 +0300

    build

--- AwesomeProject/metro.config.js
+++ AwesomeProject/metro.config.js
@@ -1,16 +1,10 @@
-const { getDefaultConfig } = require("metro-config");
+// // Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
+// module.exports = getDefaultConfig(__dirname);
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
